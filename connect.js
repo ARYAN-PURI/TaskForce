d987cb8c7f6e203b55c9959629caee3d28e801c9
ARYAN-PURI <140064234+ARYAN-PURI@users.noreply.github.com>
2024-08-16 23:21:28 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,8 +1,14 @@
 const mysql=require('mysql');
 let connect=mysql.createConnection({
+    connectionLimit : 100,
+    waitForConnections : true,
+    queueLimit :0,
     host:'sql12.freemysqlhosting.net',
     user:'sql12726075',
     password:'XuGmzSkWcK',
-    database:'sql12726075'
+    database:'sql12726075',
+    debug    :  true,
+    wait_timeout : 28800,
+    connect_timeout :10
 });
 module.exports=connect;
